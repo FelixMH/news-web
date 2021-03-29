7af8aa5f2051d1f8ae0ab31d38f5b00e41ebefd1
FelixMH <felixmtzh.fm@gmail.com>
2021-03-29 03:01:56 -0600

    changes committed

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -12,13 +12,11 @@ export class NewsService {
   constructor( private http: HttpClient ) { }
 
   getNews(): Observable<News> {
-    const options = {
-      headers: new HttpHeaders({
-        'Access-Control-Allow-Headers':'Origin, Content-Type, X-Auth-Token',
-        'Access-Control-Allow-Origin':'*',
-        'Access-Control-Allow-Methods':'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-      })
-    };
-    return this.http.get<News>('https://newsapi.org/v2/everything?q=bitcoin&apiKey=efe79552ec83456db6564c1ec305bad8', options);
+    // const options = {
+    //   headers: new HttpHeaders({
+    //     'Access-Control-Allow-Origin':'*'
+    //   })
+    // };
+    return this.http.get<News>('https://newsapi.org/v2/everything?q=bitcoin&apiKey=efe79552ec83456db6564c1ec305bad8');
   }
 }
